8fce45b0c03af7e3e4b27b9cf21e9ae1a4f91216
frontending <mymage259@gmail.com>
2022-05-24 01:03:05 +0300

    feat: add solution 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,14 @@
+const fs = require('fs')
+const path = require('path')
 
+fs.mkdir(path.join(__dirname, 'files-copy'), () => {})
+const pathCurrent = path.join(__dirname, './files')
+const pathCopy = path.join(__dirname, './files-copy')
+
+fs.readdir(pathCurrent, function (err, files) {
+    for (let file of files) {
+        const pathFile = path.join(__dirname, './files', `${file}`)
+        const pathNewFile = path.join(__dirname, './files-copy', `${file}`)
+        fs.copyFile(pathFile, pathNewFile, (err) => {})
+    }
+})
