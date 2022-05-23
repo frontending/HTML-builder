2077060dd904aa63d60e85a294fba7600e634ba6
frontending <mymage259@gmail.com>
2022-05-24 00:44:01 +0300

    feat: add solution 03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const path = require('path')
+const pathCurrent = path.join(__dirname, './secret-folder')
+console.log(pathCurrent)
+
+fs.readdir(pathCurrent, function (err, files) {
+    for (let file of files) {
+        const pathFile = path.join(__dirname, './secret-folder', `${file}`)
+        const fileName = file
+        fs.stat(pathFile, function (err, file) {
+            if (file.isFile()) {
+                //console.log(fileName.slice(0, fileName.lastIndexOf('.')))
+                //console.log(fileName.slice(fileName.lastIndexOf('.') + 1))
+                //console.log(file.size)
+                console.log(
+                    fileName.slice(0, fileName.lastIndexOf('.')) +
+                        ' - ' +
+                        fileName.slice(fileName.lastIndexOf('.') + 1) +
+                        ' - ' +
+                        file.size +
+                        'b'
+                )
+            }
+        })
+    }
+})
